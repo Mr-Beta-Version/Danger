cdac6d0702de41fcb9d3c85fafa49d2706990ce3
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-01-22 21:47:46 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -1,1 +1,13 @@
-£
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from bnsteam import menu
+    menu()
+elif bit == '32bit':
+    print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
